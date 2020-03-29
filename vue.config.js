38d80399715cff8943ed48ac6738bfe5462789c4
okjaewon <okjaewon90@gmail.com>
2020-03-29 15:49:26 +0900

    vuetify test

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,3 @@
 module.exports = {
-  devServer: {
-    disableHostCheck: true,
-  },
-
-  transpileDependencies: ['vuetify'],
-
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: false,
-    },
-  },
-}
+  transpileDependencies: ["vuetify"]
+};
